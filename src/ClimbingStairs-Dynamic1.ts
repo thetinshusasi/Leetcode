fcdd7b3e3e979b5fc86eff072ed19ea085f44893
thetinshusasi <thetinshusasi@gmail.com>
2023-11-24 06:43:07 -0400

    feat(app): 1-D Dynamic Prog climbStairs
    climbStairs

---
+++ src/ClimbingStairs-Dynamic1.ts
@@ -0,0 +1,53 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+interface Dict {
+    [key: string]: number;
+}
+function climbStairs(n: number): number {
+    const hashMap: Dict = {}
+    return countStairs(n, hashMap)
+
+};
+
+const countStairs = (currStairNo: number, dict: Dict): number => {
+    if (currStairNo === 2) {
+        return 2
+    }
+
+    if (currStairNo === 1) {
+        return 1
+    }
+
+
+    if (dict[currStairNo]) {
+        return dict[currStairNo]
+    }
+
+    const val = countStairs(currStairNo - 1, dict) + countStairs(currStairNo - 2, dict)
+    dict[currStairNo] = val
+    return val
+
+
+}
+
+console.log(climbStairs(7))
